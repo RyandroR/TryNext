84eb0d8997aeb75d7505a29b1f0c0af5d7820161
RyandroR <rubbish.lap@gmail.com>
2022-08-28 18:44:23 +0700

    meta

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,12 @@
 import styles from '../styles/Home.module.css'
 import Nav from './Nav'
 import Header from './Header'
+import Meta from './Meta'
 
 const Layout = ({children}) => {
   return (
     <div>
+        <Meta/>
         <Nav></Nav>
         <div className={styles.container}>
         <main className={styles.main}>
